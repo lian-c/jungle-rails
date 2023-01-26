ec3ce18a4c68a3ca225f92f02970b56c01dee049
lian-c <lian.chan@telus.com>
2023-01-26 01:07:10 -0800

    Display product items and category name

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
   def show
+    @products = Product.count
+
+    @categories = Category.count
+    @cat = Category.all
+  
   end
 end
