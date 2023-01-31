3b446cf0d352627ea7535e1bc37dc0a7342da41f
lian-c <lian.chan@telus.com>
2023-01-31 01:39:54 -0800

    Add test for password length and presence of name/email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,28 @@ RSpec.describe User, type: :model do
     end
   end
 
+    it "validates email must be unique but not case sensitive" do
+      user = User.create(name: "Jane Doe", email: "test@doe.com", password:"DoeJane12", password_confirmation:"DoeJane12")
+      user2 = User.create(name: "Janey Doe", email: "tEst@doE.com", password:"DoeJane12", password_confirmation:"DoeJane12")
+      expect(user2.valid?).to be(false)
+    end
 
+    it "validates presence of email" do
+      user = User.create(name: "Jane Doe", email: nil, password:"DoeJane12", password_confirmation:"DoeJane12")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+    it "validates presence of name" do
+      user = User.create(name: nil, email: "test@doe.com", password:"DoeJane12", password_confirmation:"DoeJane12")
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+
+    it "validates length of password" do
+      user = User.create(name: "Jane Doe", email: "test@doe.com", password:"janeDoe", password_confirmation:"janeDoe")
+      user.valid?
+      expect(user.errors.full_messages).to include("Password is too short (minimum is 8 characters)")
+    end
+    
   end
 end
