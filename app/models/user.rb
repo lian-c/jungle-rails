adca6d2780959d31ded043b36722c88c29396e3d
lian-c <lian.chan@telus.com>
2023-01-31 00:54:32 -0800

    Update user model to include removing white space and change to lower case

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+  before_validation :lower_case
 
-  has_secure_password 
+  has_secure_password #will check password + password confirmation, presence
+  validates :password, length: {minimum: 8}
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+end
+
+
+private
+
+def lower_case
+  self.email = email.downcase.strip if email.present?
 end
