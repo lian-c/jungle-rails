0c4d575ee9881d218c5f9a959269f1068368b5c6
lian-c <lian.chan@telus.com>
2023-01-31 01:11:30 -0800

    Add testing for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,30 +4,18 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     # validation tests/examples here
     it "validate will save" do
-      user = User.new(name: "Mary Poppins", price: 109.00, quantity: 21, category: @category)
-      expect(product.save).to be true
+      user = User.new(name: "Jane Doe", email: "test@doe.com", password:"DoeJane12", password_confirmation:"DoeJane12")
+      expect(user.save).to be true
     end
 
-    it "validates presence of name" do
-      @product = Product.create(name: nil, price: 20.99, quantity: 50, category: @category)
-      @product.valid?
-      expect(@product.errors[:name]).to eql (["can't be blank"])
+    context "When password confirmation doesn't match password" do
+    it "validates that error will be shown" do
+    user = User.new(name: "Jane Doe", email: "test@doe.com", password:"DoeJane12", password_confirmation:"DoeJane11")
+      user.valid?
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
-    it "validates presence of price" do
-      @product = Product.create(name: "Blossom Tree", quantity: 50, category: @category)
-      @product.valid?
-      expect(@product.errors[:price]).to include("can't be blank")
-    end
-    it "validates presence of quantity" do
-      @product = Product.create(price: 20.99, name: "Blossom Tree", quantity: nil, category: @category)
-      @product.valid?
-      expect(@product.errors[:quantity]).to include("can't be blank")
-    end
-    it "validates presence of category" do
-      @product = Product.create(name: "Blossom Tree", price: 20.99, quantity: 50, category: nil)
-      @product.valid?
-      expect(@product.errors[:category]).to include("can't be blank")
-    end
+  end
+
 
   end
 end
